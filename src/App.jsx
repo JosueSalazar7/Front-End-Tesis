64e1bda5cf27cde52cf2d62988775a2de148aed1
josue <josue.salazar@epn.edu.ec>
2024-04-29 17:22:40 -0500

    perfil

--- src/App.jsx
+++ src/App.jsx
@@ -41,7 +41,7 @@ function App() {
                 <PrivateRoute>
                   <Routes>
                     <Route element={<Dashboard />}>
-                      <Route path="/admin/perfil"element={<Perfil />} />
+                      <Route index element={<Perfil />} />
                       <Route path="listar" element={<Listar />} />
                       <Route path="visualizar/:id" element={<Visualizar />} />
                       <Route path="crear" element={<Crear />} />
