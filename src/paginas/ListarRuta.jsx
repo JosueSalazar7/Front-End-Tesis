90e7df4a3a08b6ca51de410df457b9e20f0014c1
josue <josue.salazar@epn.edu.ec>
2024-05-03 13:09:16 -0500

    tabla ruta

--- src/paginas/ListarRuta.jsx
+++ src/paginas/ListarRuta.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Tabla from '../componets/Tabla'
+import TablaRuta from '../componets/TablaRuta'
 
 const ListarRuta = () => {
     return (
@@ -7,7 +7,7 @@ const ListarRuta = () => {
             <h1 className='font-black text-4xl text-gray-500'>Rutas</h1>
             <hr className='my-4' />
             <p className='mb-8'></p>
-            <Tabla/>
+            <TablaRuta/>
         </div>
       )
 }
