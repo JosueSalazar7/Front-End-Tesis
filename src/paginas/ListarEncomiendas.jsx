782e71bff468da008e6576f30f10bdadc349e059
josue <josue.salazar@epn.edu.ec>
2024-06-05 19:02:09 -0500

    visualizacion

--- src/paginas/ListarEncomiendas.jsx
+++ src/paginas/ListarEncomiendas.jsx
@@ -3,7 +3,7 @@ import TablaEncomiendasPendientes from '../componets/TablaEncomiendas';
 
 const ListarEncomiendas = () => {
     return (
-        <div className="h-screen pt-48 flex items-start justify-center">
+        <div className="h-screen pt-40 flex items-start justify-center">
             <div className="max-w-9xl w-full mx-auto px-4">
                 <h1 className='font-black text-7xl text-gray-500  mb-8'>Encomiendas pendientes</h1>
                 <p className='mb-8'>Este módulo permite gestionar las encomiendas</p>
--- src/paginas/ListarViajesP.jsx
+++ src/paginas/ListarViajesP.jsx
@@ -3,7 +3,7 @@ import TablaViajesPrivadosPendientes from '../componets/TablaViajesP';
 
 const ListarViajesP = () => {
     return (
-        <div className="h-screen pt-48 flex items-start justify-center">
+        <div className="h-screen pt-40 flex items-start justify-center">
             <div className="max-w-9xl w-full mx-auto px-4">
                 <h1 className='font-black text-6xl text-gray-500 mb-8'>Viajes privados pendientes</h1>
                 <p className='mb-8'>Este módulo permite gestionar los viajes privadoss</p>
